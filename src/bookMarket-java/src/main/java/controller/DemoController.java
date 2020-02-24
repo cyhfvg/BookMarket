d54cbb9103df5e1d6c13b2bc17dd038a7a4e2eeb
cyhfvg <code.guangzhou.zhang@outlook.com>
2020-02-24 22:12:24 +0800

    spring5.x+slf4j+log4j2整合

--- src/bookMarket-java/src/main/java/controller/DemoController.java
+++ src/bookMarket-java/src/main/java/controller/DemoController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,8 +16,16 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 @RequestMapping("/home")
 public class DemoController {
+    private static Logger logger = LoggerFactory.getLogger(DemoController.class);
     @RequestMapping("/index")
     public ModelAndView index() {
+        logger.trace("trace level");
+        logger.debug("debug level");
+        logger.info("info level");
+        logger.warn("warn level");
+        logger.error("error level");
+        logger.error("字符串拼接方式一:{},此方式比+快", "logger");
+        logger.error("字符串拼接方式二:" + "logger");
         ModelAndView mv = new ModelAndView("index");
         return mv;
     }
