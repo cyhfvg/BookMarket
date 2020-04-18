84e87a3dfc4fc3aacc721a1ef197ba7adbee0a70
cyhfvg <code.guangzhou.zhang@outlook.com>
2020-04-18 22:03:21 +0800

    用户{购物车内容}已经获取到前端，下一步{渲染}

--- src/bookMarket-java/src/main/java/cn/cslg/dao/impl/BmsBookDaoImpl.java
+++ src/bookMarket-java/src/main/java/cn/cslg/dao/impl/BmsBookDaoImpl.java
@@ -2,9 +2,13 @@ package cn.cslg.dao.impl;
 
 import cn.cslg.model.BmsBook;
 import cn.cslg.dao.BmsBookDao;
+import cn.cslg.util.CollectionUtil;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Restrictions;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -23,5 +27,30 @@ public class BmsBookDaoImpl extends GenericDaoImpl<BmsBook, Long> implements Bms
         Object [] values = {0, 1};
         List<BmsBook> list = findByProperties(BmsBook.class,propertyNames, values);
         return list.size();
+    }
+
+    @Override
+    public List<BmsBook> queryBookByIds(List Ids) {
+        List<BmsBook> list = new ArrayList<>();
+        if (CollectionUtil.isEmpty(Ids)) {
+            return list;
+        }
+        for (Object id :
+                Ids) {
+            DetachedCriteria dc = DetachedCriteria.forEntityName(BmsBook.class.getName());
+            dc.add(Restrictions.eq("id", id));
+            dc.add(Restrictions.eq("deleteStatus", 0));
+            dc.add(Restrictions.eq("publishStatus", 1));
+
+            List<BmsBook> result = (List<BmsBook>) super.getHibernateTemplate().findByCriteria(dc);
+
+            if (CollectionUtil.isNotEmpty(result)) {
+                for (BmsBook book :
+                        result) {
+                    list.add(book);
+                }
+            }
+        }
+        return list;
     }
 }
