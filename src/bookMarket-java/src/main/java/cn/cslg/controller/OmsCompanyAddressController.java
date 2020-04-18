1c9bfbab856b3327256aff6866066a35866b60f9
cyhfvg <code.guangzhou.zhang@outlook.com>
2020-04-19 01:37:58 +0800

    用户{购物车中选择书籍},{订单详情},{添加地址}完成

--- src/bookMarket-java/src/main/java/cn/cslg/controller/OmsCompanyAddressController.java
+++ src/bookMarket-java/src/main/java/cn/cslg/controller/OmsCompanyAddressController.java
@@ -1,9 +1,11 @@
 package cn.cslg.controller;
 
+import cn.cslg.dto.OmsCompanyAddressAddParam;
 import cn.cslg.model.OmsCompanyAddress;
 import cn.cslg.service.OmsCompanyAddressService;
 
 import cn.cslg.bean.Response;
+import cn.cslg.util.CollectionUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * (OmsCompanyAddress)表控制层
@@ -41,6 +44,45 @@ public class OmsCompanyAddressController implements ApplicationContextAware{
     @RequestMapping(value = "/selectOne", method = RequestMethod.GET)
     public Response selectOne(Long id) {
         return new Response().failure("");
+    }
+
+    /**
+     * 获取用户地址
+     * @param userId 用户编号
+     * @return Response
+     */
+    @RequestMapping(value = "/memberAddress")
+    public Response getMemberAddress(@RequestParam("userId") long userId) {
+        Response response = new Response();
+        List<OmsCompanyAddress> list = omsCompanyAddressService.getAddressByMemberId(userId);
+        if (CollectionUtil.isEmpty(list)) {
+            return response.failure("无地址");
+        }
+        return response.success(list);
+    }
+
+    /**
+     * 用户添加一个地址
+     * @param omsCompanyAddressAddParam dto
+     * @return Response
+     */
+    @RequestMapping(value = "/addAddress", method = RequestMethod.POST)
+    public Response addAddress(@RequestBody OmsCompanyAddressAddParam omsCompanyAddressAddParam) {
+        OmsCompanyAddress omsCompanyAddress = new OmsCompanyAddress();
+
+        omsCompanyAddress.setAddressName(omsCompanyAddressAddParam.getCity() + omsCompanyAddressAddParam.getRegion());
+        omsCompanyAddress.setCity(omsCompanyAddressAddParam.getCity());
+        omsCompanyAddress.setDetailAddress(omsCompanyAddressAddParam.getDetailAddress());
+        omsCompanyAddress.setMemberId(omsCompanyAddressAddParam.getUserId());
+        omsCompanyAddress.setName(omsCompanyAddressAddParam.getName());
+        omsCompanyAddress.setPhone(omsCompanyAddressAddParam.getPhone());
+        omsCompanyAddress.setProvince(omsCompanyAddressAddParam.getProvince());
+        omsCompanyAddress.setReceiveStatus(0);
+        omsCompanyAddress.setRegion(omsCompanyAddressAddParam.getRegion());
+        omsCompanyAddress.setSendStatus(0);
+
+        omsCompanyAddressService.insert(omsCompanyAddress);
+        return new Response().success();
     }
     
     @Override
