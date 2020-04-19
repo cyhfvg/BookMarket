2f0d805a86add42d8d1b306352491f2f6ca794cd
cyhfvg <cnzhanggz@outlook.com>
2020-04-19 11:32:16 +0800

    管理员客户端{框架搭建完毕}

---
+++ src/bookMarket-node-admin/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // node端 配置项
+    port: 8091,
+    project_path: __dirname,
+    // 后台api地址 配置项
+    apiHost: "http://127.0.0.1",
+    apiPort: 8080,
+    apiModuleName: "bookMarket-java",
+}
