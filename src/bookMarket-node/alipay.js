2502386b76d6e9b56450c69ae8e15017ce1c8990
cyhfvg <cnzhanggz@outlook.com>
2020-05-25 08:56:05 +0800

    fix secret bugs

--- src/bookMarket-node/alipay.js
+++ src/bookMarket-node/alipay.js
@@ -6,7 +6,7 @@ exports.charge = async function (tradeNo, charge, callback) {
   // 调用 setMethod 并传入 get，会返回可以跳转到支付页面的 url，否则返回的是一个表单的 html 片段
   formData.setMethod('get');
 
-  formData.addField('notifyUrl', 'http://47.107.54.79:8081/notify'); // 当支付完成后，支付宝主动向我们的服务器发送回调的地址
+  formData.addField('notifyUrl', 'http://192.168.xx.xx:8081/notify'); // 当支付完成后，支付宝主动向我们的服务器发送回调的地址
   formData.addField('returnUrl', 'http://127.0.0.1:8081/views/memberInfo'); // 当支付完成后，当前页面跳转的地址
   formData.addField('bizContent', {
     outTradeNo: tradeNo,
